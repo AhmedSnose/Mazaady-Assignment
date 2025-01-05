37b664df9735f9b47e58a9efbc0eb03b63d133a9
Ahmed Snose <mrahmednasr63@gmail.com>
2025-01-05 11:01:07 +0200

    update NEXT_PUBLIC_APP_URL env

--- services/CategoryService.ts
+++ services/CategoryService.ts
@@ -1,8 +1,16 @@
-const BASE_URL = process.env.NEXT_PUBLIC_APP_URL || process.env.NEXT_PUBLIC_VERCEL_URL;
 export class CategoryService {
+  private getBaseUrl() {
+    if (typeof window !== 'undefined') {
+      return '';
+    }
+    const BASE_URL = process.env.NEXT_PUBLIC_VERCEL_URL;
+    return BASE_URL;
+  }
+
   async getAllCategories() {
     try {
-      const response = await fetch(BASE_URL+"api/categories");
+      const baseUrl = this.getBaseUrl();
+      const response = await fetch(`${baseUrl}/api/categories`);
       const data = await response.json();
       return data;
     } catch (error) {
@@ -13,7 +21,8 @@ export class CategoryService {
 
   async fetchProperties(categoryId: number) {
     try {
-      const response = await fetch(`api/properties?cat=${categoryId}`);
+      const baseUrl = this.getBaseUrl();
+      const response = await fetch(`${baseUrl}/api/properties?cat=${categoryId}`);
       const data = await response.json();
       return data;
     } catch (error) {
@@ -22,4 +31,3 @@ export class CategoryService {
     }
   }
 }
-
